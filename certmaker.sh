3f722475cc453a94f74e378cf681a7a60c58182f
Michael Vogt <mvo@debian.org>
2013-06-04 14:31:14 +0200

    use first server domain as filename

--- certmaker.sh
+++ certmaker.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/bin/sh
+
+set -e
 
 KEYSIZE=4096
 
@@ -22,28 +24,30 @@ organizationalUnitName  = ZIMK
 "
 
 # script
-INPUT_FILE=`/bin/tempfile`
+INPUT_FILE=tmp_input
 
 # ask for domain list
 whiptail --inputbox "Bitte geben Sie eine kommaseparierte Liste von Domains ein, die im Zertifikat enthalten sein sollen:" 10 60 2> $INPUT_FILE
-DOMAINS=`cat $INPUT_FILE`
+DOMAINS=$(cat $INPUT_FILE)
 COUNTER=0
-for domain in ${DOMAINS//,/ } ; do
-	CONFIG=`echo -e "$CONFIG\\\\n$COUNTER.commonName = $domain"`
-	COUNTER=`expr $COUNTER + 1`
+for domain in $(echo $DOMAINS | tr ',' '\ '); do
+        if [ -z "$SERVERNAME" ]; then
+                SERVERNAME="$domain"
+        fi
+	CONFIG="$CONFIG\\n$COUNTER.commonName = $domain"
+	COUNTER=$((COUNTER + 1))
 done
 
 # ask for email address
 whiptail --inputbox "Bitte geben Sie eine E-Mail Adresse für das Zertifikat an:" 10 60 "webmaster@uni-trier.de" 2> $INPUT_FILE
-EMAIL=`cat $INPUT_FILE`
-CONFIG=`echo -e "$CONFIG\\\\nemailAddress = $EMAIL"`
+EMAIL="$(cat $INPUT_FILE)"
+CONFIG="$CONFIG\nemailAddress = $EMAIL"
 
-CONFIG_FILE=`/bin/tempfile`
+CONFIG_FILE=tmp_config.cnf
 echo "$CONFIG" > $CONFIG_FILE
-/bin/dd if=/dev/random of=./server.rand bs=$KEYSIZE count=1 2>/dev/null
-/usr/bin/openssl genrsa -out server.key $KEYSIZE
-/usr/bin/openssl req -new -key server.key -out server.csr -config $CONFIG_FILE
-/usr/bin/openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt	
-/bin/rm -f ./server.rand
-/bin/rm $INPUT_FILE
-/bin/rm $CONFIG_FILE
+dd if=/dev/random of=./${SERVERNAME}.rand bs=$KEYSIZE count=1 2>/dev/null
+openssl genrsa -out ${SERVERNAME}.key $KEYSIZE
+openssl req -new -key ${SERVERNAME}.key -out ${SERVERNAME}.csr -config $CONFIG_FILE
+openssl x509 -req -days 365 -in ${SERVERNAME}.csr -signkey ${SERVERNAME}.key -out ${SERVERNAME}.crt	
+
+trap "rm -f ./${SERVERNAME}.rand; rm $INPUT_FILE; rm $CONFIG_FILE" EXIT INT TERM
