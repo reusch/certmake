923725cd68f9204a1734469b97c4d343a7919192
Michael Vogt <mvo@debian.org>
2013-06-17 18:29:42 +0200

    add mail and multiple escalation levels

--- tools/check-expire-date.sh
+++ tools/check-expire-date.sh
@@ -2,8 +2,33 @@
 
 set -e
 
-# minimal warning days
-MIN_DAYS=42
+# first warning level
+MIN_WARN_DAYS=42
+WARN_MAILTO=michael.vogt@uni-trier.de
+#WARN_MAILTO=shen@uni-trier.de
+
+# escalate
+MIN_ESCALATE_DAYS=14
+WARN_ESCALATE_MAILTO=zentrale-systeme@zimk.uni-trier.de
+
+# really escalate(!)
+MIN_MIN_DAYS=7
+MIN_MIN_MAILTO=leitung@zimk.uni-trier.de
+
+mail_warning() {
+    MAILTO="$1"
+    SUBJECT="$2"
+    BODY="$3"
+    
+    # debug
+    #echo "To: $MAILTO"
+    #echo "Subject: $SUBJECT"
+    #echo
+    #echo "$BODY"
+
+    # really send mail
+    echo "$BODY" | mail $MAILTO -s "$SUBJECT" 
+}
 
 check_server_cert_expire() {
   SERVER="$1"
@@ -12,17 +37,36 @@ check_server_cert_expire() {
   # FIXME: add check for warnings from openssl too
   OUT=$(openssl s_client -showcerts  -connect ${SERVER}:${PORT} \
          < /dev/null 2>/dev/null | \
-         openssl x509 -noout -subject -dates )
+         openssl x509 -noout -subject -dates)
 
   END_DATE=$(echo "$OUT" | grep notAfter | cut -f2 -d=)
   END_TIME_EPOCH=$(date -d "$END_DATE" +%s)
     
   NOW_EPOCH=$(date +%s)
-  WARN_EPOCH=$((NOW_EPOCH + 60*60*24*${MIN_DAYS}))
-  
-  if [ "$END_TIME_EPOCH" -lt "$WARN_EPOCH" ]; then
+
+  if [ "$END_TIME_EPOCH" -lt $((NOW_EPOCH + 60*60*24*${MIN_MIN_DAYS})) ]; 
+  then
+      mail_warning "$WARN_MAILTO" \
+          "CERT EXPIRES SOON: $SERVER:$PORT" \
+          "Cert for $SERVER:$PORT expires in less than $MIN_MIN_DAYS days
+Expire date is $END_DATE"
+    return 1
+  elif [ "$END_TIME_EPOCH" -lt $((NOW_EPOCH + 60*60*24*${MIN_ESCALATE_DAYS})) ]; 
+  then
+      mail_warning "$WARN_MAILTO" \
+          "Cert expires soon: $SERVER:$PORT" \
+          "Cert for $SERVER:$PORT expires in less than $MIN_ESCALATE_DAYS days
+Expire date is $END_DATE"
+    return 1
+  elif [ "$END_TIME_EPOCH" -lt $((NOW_EPOCH + 60*60*24*${MIN_WARN_DAYS})) ]; 
+  then
+      mail_warning "$WARN_MAILTO" \
+          "Cert expires soon: $SERVER:$PORT" \
+          "Cert for $SERVER:$PORT expires in less than $MIN_WARN_DAYS days
+Expire date is $END_DATE"
     return 1
   fi
+
   return 0
 }
 
@@ -40,8 +84,5 @@ if [ -z "$PORT" ]; then
 fi
 
 # warn if needed
-if ! check_server_cert_expire "$SERVER" "$PORT"; then
-    echo "WANING: cert for $SERVER:$PORT expires in less than $MIN_DAYS days "
-    echo "Expire date is $END_DATE"
-fi
+check_server_cert_expire "$SERVER" "$PORT"
 
