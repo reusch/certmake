e029e945bc7f9e6c4d9ffc4098e63ac98511f989
Michael Vogt <mvo@debian.org>
2013-06-17 23:02:34 +0200

    fix parsing for multiple lines

--- tools/find-all-ssl-hosts.sh
+++ tools/find-all-ssl-hosts.sh
@@ -11,14 +11,19 @@ HERE=$(dirname $0)
 PORTS="443,465,636,993,995"
 HOSTS="136.199.8.1-254 136.199.199.1-254"
 
-OUT=$(nmap --open -sT -P0 -p $PORTS $HOSTS -oG -|grep 'Ports:')
+# gather data
+nmap --open -sT -P0 -p $PORTS $HOSTS -oG - | grep 'Ports:' ssl_hosts.txt
 
-HOST_IP=$(echo "$OUT" | cut -f1 | cut -d' ' -f2)
-PORTS=$(echo "$OUT" | cut -f2 | cut -f2 -d: )
+# now go over ssl_hosts.txt and parse it
+exec<ssl_hosts.txt
+while read LINE; do
+    HOST_IP=$(echo "$LINE" | cut -f1 | cut -d' ' -f2)
+    PORTS=$(echo "$LINE" | cut -f2 | cut -f2 -d: )
 
-for port_string in $PORTS; do
-    port=$(echo $port_string | cut -d/ -f1)
-    echo check_server_cert_expire $HOST_IP $port
-    check_server_cert_expire $HOST_IP $port
+    for port_string in $PORTS; do
+        port=$(echo $port_string | cut -d/ -f1)
+        echo check_server_cert_expire $HOST_IP $port
+        check_server_cert_expire $HOST_IP $port
+    done
 done
 
