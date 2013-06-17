56e46fa4d018cad533a8e64382b39578b6746f50
Michael Vogt <mvo@debian.org>
2013-06-17 15:31:34 +0200

    import-dfn-pem.sh: initial version

---
+++ import-dfn-pem.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -e
+
+SIGNED_CRT="$1"
+if [ ! -f "$SIGNED_CRT" ]; then
+    echo "need the dfn pem file as the first argument"
+    exit 1
+fi
+
+TARGET_DIR="$2"
+if [ ! -d "$TARGET_DIR" ]; then
+    echo "need the server directory as the second argument"
+    exit 1
+fi
+
+install -m 0600 "$SIGNED_CRT" "${TARGET_DIR}/server.cst"
+
+# generate various chains
+cd "$TARGET_DIR"
+
+#TODO: generate various chains
+
