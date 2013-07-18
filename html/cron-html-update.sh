61e6d93b3b24d7babb32e8b6a87f1c4d22605dcb
Michael Vogt <mvo@debian.org>
2013-07-18 14:44:09 +0200

    make cron deploy more flexible

--- html/cron-html-update.sh
+++ html/cron-html-update.sh
@@ -5,6 +5,14 @@ set -e
 HERE=$(dirname $0)
 cd $HERE
 
-../tools/verify-all-ssl-certs.sh > cert-status.txt
-./convert-cert-status.py cert-status.txt > cert-status.html.tmp
-mv cert-status.html.tmp cert-status.html
+STATIC="css img js"
+TARGETDIR=/var/www/certstatus
+TARGET=$TARGETDIR/index.html
+
+# deploy static stuff
+cp -ar $STATIC $TARGETDIR
+
+# deploy info
+../tools/verify-all-ssl-certs.sh > ${TARGET}
+./convert-cert-status.py cert-status.txt > ${TARGET}.tmp
+mv ${TARGET}.tmp $TARGET
